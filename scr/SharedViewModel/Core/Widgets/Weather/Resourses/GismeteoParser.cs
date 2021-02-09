68aae23cbc1cc1c85ea0d24b28a9de7c841849b4
Филатов Егор <egorwolf2k18@mail.ru>
2021-02-09 15:04:18 +0300

    Создан виджет погоды и добавленна возможность смотреть температуру и состояние погоды.

---
+++ scr/SharedViewModel/Core/Widgets/Weather/Resourses/GismeteoParser.cs
@@ -0,0 +1,88 @@
+﻿//-----------------------------------------------------------------------------
+// <copyright file="IParser.cs" company="SW Okolo IT">
+//  Copyright (c) SW Okolo IT. All rights reserved.
+// </copyright>
+// <summary> 
+// Реализация класса для парсинга на сайте Gismeteo.
+// </summary> 
+//-----------------------------------------------------------------------------
+using AngleSharp.Dom;
+using AngleSharp.Html.Dom;
+using Parser.Base;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ViewModel.Core.Widgets.Weather.Resourses
+{
+    /// <summary>
+    /// Парсер для сайта Gismeteo.
+    /// </summary>
+    internal class GismeteoParser : IParser<string[]>
+    {
+        readonly string BASE_URL;
+        string filter;
+
+        /// <summary>
+        /// Создает экземпляр класса GismeteoParser.
+        /// </summary>
+        /// <param name="settings">
+        /// Текущие настройки страницы для парсинга.
+        /// </param>
+        public GismeteoParser()
+        {
+            this.filter = default;
+            BASE_URL = "https://www.gismeteo.ru/weather-fryazino-12648/now/";
+        }
+
+        /// <summary>
+        /// Передаёт адрес страницы.
+        /// </summary>
+        public string URL {
+            get => BASE_URL;
+        }
+
+        /// <summary>
+        /// Ищет данные по фильтру.
+        /// </summary>
+        /// <param name="document">Код страницы.</param>
+        /// <returns>Данные со страницы.</returns>
+        public string[] Parshe(IHtmlDocument document)
+        {
+            List<string> list = new List<string>();
+
+            filter = "js_value tab-weather__value_l";
+            LookingForDataFromClassByFilter(document, "span", ref list);
+            filter = "tip _top _center";
+            LookingForDataFromClassByFilter(document, "span", ref list);
+
+            return list.ToArray();
+        }
+
+        /// <summary>
+        /// Добавляет данные в коллекцию.
+        /// </summary>
+        /// <param name="items">Перечислитель.</param>
+        /// <param name="list">Список для записи данных.</param>
+        private void DataLoader(IEnumerable<IElement> items, ref List<string> list)
+        {
+            foreach (var item in items)
+                list.Add(item.TextContent);
+        }
+
+        /// <summary>
+        /// Поиск данных изз класса по филитру и загружает их в список.
+        /// </summary>
+        /// <param name="document">Код страницы.</param>
+        /// <param name="className">Имя CSS абзаца.</param>
+        /// <param name="list">Список для записи данных.</param>
+        private void LookingForDataFromClassByFilter(IHtmlDocument document,
+                                                     string className,
+                                                     ref List<string> list)
+        {
+            IEnumerable<IElement> items = document.QuerySelectorAll(className)
+                    .Where(item => item.ClassName != null &&
+                     item.ClassName.Contains(filter));
+            DataLoader(items, ref list);
+        }
+    }
+}
---
+++ scr/SharedViewModel/Core/Widgets/Weather/WeatherWidgetManager.cs
@@ -0,0 +1,93 @@
+﻿//-----------------------------------------------------------------------------
+// <copyright file="WeatherWidgetManager.cs" company="SW Okolo IT">
+//  Copyright (c) SW Okolo IT. All rights reserved.
+// </copyright>
+// <summary> 
+// Файл с реализацией класса WeatherWidgetManager.
+// </summary> 
+//-----------------------------------------------------------------------------
+using Parser;
+using System;
+using ViewModel.Core.Widgets.Weather.Resourses;
+
+namespace ViewModel.Core.Widgets.Weather
+{
+    /// <summary>
+    /// Управление виджетом погоды.
+    /// </summary>
+    public sealed class WeatherWidgetManager
+    {
+        string temperature;
+        string weatherStatus;
+        string weatherPictureName;
+
+        /// <summary>
+        /// Создает экземпляр класса WeatherWidgetManager.
+        /// </summary>
+        public WeatherWidgetManager()
+        {
+            temperature = default;
+            weatherStatus = default;
+            //weatherPictureName = "Textures/";
+        }
+
+        /// <summary>
+        /// Обновляет состояние о погоде.
+        /// </summary>
+        /// <param name="data">Данные о погоде.</param>
+        private void ParserWorkerOnNewData(string[] data)
+        {
+            if (data.Length > 2) {
+                temperature   = data[0].Trim();
+                weatherStatus = data[2].Trim();
+                //ConditionAnalysis();
+            }
+            else {
+                temperature = "Нет данных.";
+                weatherStatus = "Неполадки с подключением.";
+            }
+        }
+
+        /// <summary>
+        /// Анализ состояния для выбора изображения на спрайт.
+        /// </summary>
+        /*private void ConditionAnalysis()
+        {
+            if (weatherStatus == null)
+                throw new ArgumentNullException("weatherStatus == null");
+
+            if (weatherStatus.Contains("снег"))
+                weatherPictureName += "snow.png";
+            else
+                weatherPictureName += "cloudy.png";
+        }*/
+
+        /// <summary>
+        /// Загружает данные о погоде.
+        /// </summary>
+        public void LoadData()
+        {
+            var parserWorker = new ParserWorker<string[]>(new GismeteoParser());
+            parserWorker.OnNewData += ParserWorkerOnNewData;
+            parserWorker.Start();
+        }
+
+        /// <summary>
+        /// Возвращает температуру.
+        /// </summary>
+        /// <returns>Температура.</returns>
+        public string GetTemperature() => temperature;
+
+        /// <summary>
+        /// Возвращает статус погоды.
+        /// </summary>
+        /// <returns>Статус погоды.</returns>
+        public string GetState() => weatherStatus;
+
+        /// <summary>
+        /// Возвращает путь к файлу спрайта.
+        /// </summary>
+        /// <returns>Путь к файлу спрайта.</returns>
+        public string GetPathImage() => weatherPictureName;
+    }
+}
